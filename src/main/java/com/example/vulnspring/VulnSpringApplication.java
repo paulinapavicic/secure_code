b2b11fab482f6cbc28f27e717a06efa52b10726b
Paulina Pavičić <ppavicic@racunarstvo.hr>
2026-03-15 22:41:07 +0100

    .

--- src/main/java/com/example/vulnspring/VulnSpringApplication.java
+++ src/main/java/com/example/vulnspring/VulnSpringApplication.java
@@ -2,6 +2,8 @@ package com.example.vulnspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VulnSpringApplication {
@@ -9,7 +11,7 @@ public class VulnSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VulnSpringApplication.class, args);
 	}
-	
+
 	
 
 }
